ad0c968975fbdc50d399ff304c5f1bd4e019e787
deepakappways <deepakb@waysapp.in>
2019-06-25 12:03:16 +0530

    Single Expression Function implementation

--- src/chapter2/SingleExpressionFunction.kt
+++ src/chapter2/SingleExpressionFunction.kt
@@ -6,4 +6,15 @@ fun main() {
 /*
 * Single Expression function is which can return expression only in one line like following
 * */
-fun muliplyWith2(number:Int)=number*2
+
+/*
+* Inferred type mean compiler now the return and value assigned to val or var in kotlin
+* compile now the data type from expression
+* if he don't able to understand type then it show error for that expression
+* Example
+* fun muliplyWith2(number: Int):Int = number * 2
+* As you can see I am using return type in following fun implementation
+* */
+
+
+fun muliplyWith2(number: Int) = number * 2
