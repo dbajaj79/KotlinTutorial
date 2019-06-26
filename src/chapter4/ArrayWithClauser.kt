8d79dac4adfde0c2c2d26a24bc2280bed96f635a
deepakappways <deepakb@waysapp.in>
2019-06-26 11:41:08 +0530

    Implement Array with clauser

--- src/chapter4/ArrayWithClauser.kt
+++ src/chapter4/ArrayWithClauser.kt
@@ -1,2 +1,19 @@
 package chapter4
 
+fun main() {
+arrayWithClauser()
+}
+fun arrayWithClauser()
+{
+    val array = Array(3){i -> i*3 }
+    val stringArray = Array<String>(5){i-> "Item ${i*5}"}
+    for(i in array)
+    {
+        println(i)
+    }
+    for (i in stringArray)
+    {
+        println(i)
+    }
+}
+
