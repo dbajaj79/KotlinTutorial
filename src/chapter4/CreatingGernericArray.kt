a0c2337a5c8bbb67d7bf57cabc2199063287cd64
deepakappways <deepakb@waysapp.in>
2019-06-26 11:04:10 +0530

    Creating Generic Array in Kotlin

--- src/chapter4/CreatingGernericArray.kt
+++ src/chapter4/CreatingGernericArray.kt
@@ -1,13 +1,18 @@
 package chapter4
 
 fun main() {
-    println(createEmptyArray()[0])
+    println(createEmptyArray())
 }
 
 fun createEmptyArray():Array<String>
 {
     var emptyArray = emptyArray<String>()
-
+    val names = Array<String>(size = 5,init = {index-> "Item${index*5}"})
+    emptyArray = names
+    for(i in emptyArray)
+    {
+        println(i)
+    }
 
     return emptyArray
 }
